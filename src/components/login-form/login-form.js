a56574fb6ebc1d42a0babd473694e6810ae98963
Philipp <filya.kazakov@yandex.ru>
2023-02-15 18:01:20 +0300

    Готовая форма авторизации

--- src/components/login-form/login-form.js
+++ src/components/login-form/login-form.js
@@ -20,7 +20,7 @@ class LoginForm extends React.Component {
           <MDBRow className="g-0">
             <MDBCol md="6">
               <MDBCardImage
-                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                src="../others/images/auth.png"
                 alt="login form"
                 className="rounded-start w-100"
               />
@@ -34,52 +34,41 @@ class LoginForm extends React.Component {
                     icon="cubes fa-3x me-3"
                     style={{ color: "#ff6219" }}
                   />
-                  <span className="h1 fw-bold mb-0">Logo</span>
+                  <span
+                    className="h1 fw-bold mb-0"
+                    style={{
+                      color: "#ff611a",
+                      marginTop: "40px",
+                    }}
+                  >
+                    РАМЗЭС 3.0
+                  </span>
                 </div>
 
-                <h5
-                  className="fw-normal my-4 pb-3"
-                  style={{ letterSpacing: "1px" }}
-                >
-                  Sign into your account
-                </h5>
+                <h5 style={{ marginTop: "40px" }}>Авторизуйтесь</h5>
 
                 <MDBInput
                   wrapperClass="mb-4"
-                  label="Email address"
+                  label="Логин пользователя"
                   id="formControlLg"
                   type="email"
                   size="lg"
                 />
                 <MDBInput
                   wrapperClass="mb-4"
-                  label="Password"
+                  label="Пароль"
                   id="formControlLg"
                   type="password"
                   size="lg"
                 />
 
-                <MDBBtn className="mb-4 px-5" color="dark" size="lg">
-                  Login
+                <MDBBtn
+                  className=""
+                  size="lg"
+                  style={{ backgroundColor: "#ff611a", border: "none" }}
+                >
+                  Войти
                 </MDBBtn>
-                <a className="small text-muted" href="#!">
-                  Forgot password?
-                </a>
-                <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
-                  Don't have an account?{" "}
-                  <a href="#!" style={{ color: "#393f81" }}>
-                    Register here
-                  </a>
-                </p>
-
-                <div className="d-flex flex-row justify-content-start">
-                  <a href="#!" className="small text-muted me-1">
-                    Terms of use.
-                  </a>
-                  <a href="#!" className="small text-muted">
-                    Privacy policy
-                  </a>
-                </div>
               </MDBCardBody>
             </MDBCol>
           </MDBRow>
